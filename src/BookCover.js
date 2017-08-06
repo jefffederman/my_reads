35ceae5dbcd37c29d1bc9f4b74082c9b216fe806
Jeff Federman <jefffederman@gmail.com>
2017-08-06 15:46:43 -0400

    Best effort at normalizing book cover presentation

--- src/BookCover.js
+++ src/BookCover.js
@@ -7,7 +7,10 @@ export default function BookCover(props) {
       style={{
         width: 128,
         height: 193,
-        backgroundImage: `url("${props.thumbnail}")`
+        backgroundImage: `url("${props.thumbnail}")`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPositionX: 'center'
       }}
     ></div>
   );
